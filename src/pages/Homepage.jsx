49ee5f9a87bfaaa874a463cc2642592f78ce3000
Pratham <prattandon1507@gmail.com>
2025-02-13 19:19:13 +0530

    initial commit

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,15 @@
+import AppNav from "../Components/AppNav";
+import PageNav from "../Components/PageNav";
+import { Link } from "react-router-dom";
+function Homepage() {
+  return (
+    <div>
+      <PageNav />
+      <AppNav />
+      <h1 className="test">WorldWise</h1>
+      <Link to="/app">Go to the App!</Link>
+    </div>
+  );
+}
+
+export default Homepage;
